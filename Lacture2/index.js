d63cd4b59200694ca28b446ee0d1773e9c36e941
ajaykumarguptaa <theincredibleknowledge@gmail.com>
2025-07-22 23:01:51 +0530

    add json res.json

--- Lacture2/index.js
+++ Lacture2/index.js
@@ -4,10 +4,19 @@ require("dotenv").config();
 const app = express();
 // const port = 3000;
 
+const data = { firstName: "John", lastName: "Doe" };
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/data", (req, res) => {
+  res.json([
+    { firstName: "John", lastName: "Doe" },
+    { firstName: "Anna", lastName: "Smith" },
+    { firstName: "Peter", lastName: "Jones" },
+  ]);
+});
 app.get("/twiter", (req, res) => {
   res.send(`hello how are you ?
       <h1>hello how are you.....</h1>
